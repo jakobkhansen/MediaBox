c56b7c9fc2eb52e3875d6b6e7aa2bf92c81b0174
jakobkhansen <jakob.hansen@hotmail.no>
2019-07-11 21:19:13 +0200

    Code rework and requirements specification

--- src/media_player/player.py
+++ src/media_player/player.py
@@ -1,45 +1,7 @@
-import vlc
 import time
-import keyboard
 import os
 from media_player.stream_to_pi import play_pi
 from utils.utils import *
-
-class MediaPlayer:
-    def __init__(self):
-        self._instance = vlc.Instance(["--video-on-top"])
-        self._player = self._instance.media_player_new()
-        self._player.set_fullscreen(True)
-        self._enable_hotkeys()
-
-    def start_playback(self, media_link):
-        media = self._instance.media_new(media_link)
-        self._player.set_media(media)
-        self._player.play()
-
-    def toggle_pause(self):
-        self._player.pause()
-
-    # def quit(self):
-        # self._player.stop()
-
-    def seek_forward(self):
-        current_time = self._player.get_time()
-        self._player.set_time(current_time + 10000)
-
-    def seek_backward(self):
-        current_time = self._player.get_time()
-        self._player.set_time(current_time - 10000)
-
-    def _enable_hotkeys(self):
-        hotkeys = {
-            "space":self.toggle_pause,
-            "right":self.seek_forward,
-            "left":self.seek_backward,
-        }
-
-        for hotkey in hotkeys.keys():
-            keyboard.add_hotkey(hotkey, hotkeys[hotkey])
 
 def play_locally(video_path):
     print("play locally")
@@ -54,8 +16,6 @@ def select_videofile(directory):
 
     clear_screen()
     return file_menu(videos)
-
-
 
 
 def player_menu():
